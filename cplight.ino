bdd1c6f9c6cd46fde5d527d42bd8c5c136dfec52
liangwei <liangw1024@qq.com>
2020-07-19 19:47:00 +0800

    v1.0

---
+++ cplight.ino
@@ -0,0 +1,97 @@
+/*
+ * Arduino interface for the use of WS2812 strip LEDs
+ * Uses Adalight protocol and is compatible with Boblight, Prismatik etc...
+ * "Magic Word" for synchronisation is 'Ada' followed by LED High, Low and Checksum
+ *liangw1024@qq.com
+ * @date: 11/22/2019
+ */
+#include "FastLED.h"
+#include <WS2812FX.h>
+#define NUM_LEDS 131 
+#define DATA_PIN 3 
+
+// Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
+#define serialRate 115200
+
+// Adalight sends a "Magic Word" (defined in /etc/boblight.conf) before sending the pixel data
+uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
+
+// Initialise LED-array
+CRGB leds[NUM_LEDS];
+
+//control 
+String command = "";
+#define LED_CONTROL_PIN 7
+#define BLED 1
+WS2812FX ws2812fx = WS2812FX(BLED, LED_CONTROL_PIN, NEO_GRB + NEO_KHZ800);
+void setup() {
+
+ ws2812fx.init();
+  ws2812fx.setBrightness(120);
+  ws2812fx.setSpeed(200);
+  ///ws2812fx.setColor(0x007BFF);
+  
+  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  ws2812fx.start();
+  
+  // Use NEOPIXEL to keep true colors
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  // Initial RGB flash
+  LEDS.showColor(CRGB(255, 0, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 255, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 255));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 0));
+  
+  Serial.begin(serialRate);
+  // Send "Magic Word" string to host
+  Serial.print("Ada\n");
+}
+
+void loop() { 
+  ws2812fx.service();
+  // Wait for first byte of Magic Word
+  for(i = 0; i < sizeof prefix; ++i) {
+    waitLoop: while (!Serial.available()) ;;
+    // Check next byte in Magic Word
+    if(prefix[i] == Serial.read()) continue;
+    // otherwise, start over
+    i = 0;
+    goto waitLoop;
+  }
+  
+  // Hi, Lo, Checksum  
+  while (!Serial.available()) ;;
+  hi=Serial.read();
+  while (!Serial.available()) ;;
+  lo=Serial.read();
+  while (!Serial.available()) ;;
+  chk=Serial.read();
+  
+  // If checksum does not match go back to wait
+  if (chk != (hi ^ lo ^ 0x55)) {
+    i=0;
+    goto waitLoop;
+  }
+  
+  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+  // Read the transmission data and set LED values
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    byte r, g, b;    
+    while(!Serial.available());
+    r = Serial.read();
+    while(!Serial.available());
+    g = Serial.read();
+    while(!Serial.available());
+    b = Serial.read();
+    leds[i].r = r;
+    leds[i].g = g;
+    leds[i].b = b;
+  }
+  
+  // Shows new values
+  FastLED.show();
+}
